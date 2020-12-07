7c3a3565377c82ed6b6774d4c222be2b8f9063ef
Marwan Saad <marwan10salem@hotmail.com>
2020-12-07 21:41:39 +0200

    backup

--- warp.py
+++ warp.py
@@ -2,57 +2,91 @@ import cv2
 import numpy as np
 
 # warp each position into the position in the output frame
+
+
 def warp_first_image_points(H_mat, imgA):
     '''
     Computes the warped coordinates for an image, using homography
     matrix H
     '''
-    W, H, C= imgA.shape
+    W, H, C = imgA.shape
 
-    mapped_points = []
+    #mapped_points = []
+    x_values = []
+    y_values = []
+
+
     for x in range(W):
         for y in range(H):
-            p = np.array([x, y, 1]).reshape(3,1)
+            p = np.array([x, y, 1]).reshape(3, 1)
             p_dash = np.matmul(H_mat, p)
             x_dash, y_dash, w = p_dash[:, 0]
             x_dash /= w
             y_dash /= w
-            mapped_points.append(np.array([x_dash, y_dash]))
-    #there are W*H mapped points
-    mapped_points =np.array(mapped_points).reshape(W, H)
-    return mapped_points
+            #mapped_points.append((x_dash, y_dash))
+            x_values.append(x_dash)
+            y_values.append(y_dash)
+    # there are W*H mapped points
+    #mapped_points = np.array(mapped_points).reshape(2 * W, H)
+    x_values = np.array(x_values).reshape(W, H)
+    y_values = np.array(y_values).reshape(W, H)
+
+    return x_values, y_values
 
 
-def apply_forward_warp(first_image, mapped_points, second_image_shape):
-    W, H, C = second_image_shape
-    warped_image = np.zeros(second_image_shape, dtype=np.uint16)
-    averaging_weight = np.zeros(second_image_shape, dtype=np.uint16)
+def apply_forward_warp(first_image, mapped_points, output_image_shape):
+    W, H, C = output_image_shape
+    warped_image = np.zeros(output_image_shape, dtype=np.uint16)
+    #mask = np.bool(output_image_shape)
+    averaging_weight = np.zeros(output_image_shape, dtype=np.uint16)
 
     W_src, H_src, C_src = first_image.shape
 
+    x_values, y_values = mapped_points
+    print('before loop1')
     for x_src in range(W_src):
         for y_src in range(H_src):
             for c_src in range(C_src):
-                x, y = mapped_points[x_src, y_src]
-                try:
+                x, y = x_values[x_src, y_src], y_values[x_src, y_src]
+                if x == int(x) and y == int(y) and (x < W and x >= 0 and x < H and y >= 0):
                     warped_image[x, y, c_src] += first_image[x_src, y_src, c_src]
                     averaging_weight[x, y, c_src] += 1
-                except:
+                else:
                     # exception will be raised if x or y are sub pixel values or out x>=W y>=H
-                    pass
-    
-    for x in range(W):
-        for y in range(H):
-            for c in range(C):
-                w = averaging_weight[x, y, c]
-                if w > 1:
-                    warped_image[x, y, c] //= w
-    return warped_image
-    #apply splatting
-    
+                    x_ceil = int(np.ceil(x))
+                    x_floor = int(np.floor(x)) #, dtype=np.int16)
+                    y_ceil = int(np.ceil(y))
+                    y_floor = int(np.floor(y))
+                    ptA = (x_ceil, y_floor)
+                    ptB = (x_ceil, y_ceil)
+                    ptC = (x_floor, y_floor)
+                    ptD = (x_floor, y_ceil)
+                    pts = [ptA, ptB, ptC, ptD]
+                    for pt in pts:
+                        if pt[0] >= W or pt[0] < 0 or pt[1] >= H or pt[1] < 0:
+                            continue
+                        ### what in the world is splatting
+                        warped_image[pt[0], pt[1], c_src] += first_image[x_src, y_src, c_src]
+                        averaging_weight[pt[0], pt[1], c_src] += 1
+    print('before loop2')
+    #empty_pos
+    # for x in range(W):
+    #     for y in range(H):
+    #         for c in range(C):
+    #             w = averaging_weight[x, y, c]
+    #             if w > 1:
+    #                 warped_image[x, y, c] //= w
+                
+    #             if w !=0:
+    #                 pass
 
-    
-def inverse_warp(H, warped_image, source_image):
+    warped_image =np.where(averaging_weight > 1, warped_image//averaging_weight, warped_image)
+    warped_image = warped_image.astype(np.uint8)
+    return warped_image, averaging_weight[:, :, 0]
+    # apply splatting
+
+
+def inverse_warp(H, warped_image, source_image, mask):
     '''
     Fill out the holes (black pixels) in the warped image
     '''
@@ -60,27 +94,35 @@ def inverse_warp(H, warped_image, source_image):
     W, H, C = warped_image.shape
     W_src, H_src, C_src = source_image.shape
 
-    
     for c in range(C):
         for x in range(W):
             for y in range(H):
-                pixel_value = warped_image[x , y, c]
-                if pixel_value == 0:
-                    p_dash = np.array([x, y, 1]).reshape(3,1)
+                pixel_value = warped_image[x, y, c]
+                if mask[x, y] == 0:
+                    p_dash = np.array([x, y, 1]).reshape(3, 1)
                     p_src = np.matmul(H_inv, p_dash)
                     x_src, y_src, w_src = p_src[:, 0]
                     x_src = x_src/w_src
                     y_src = y_src/w_src
-                    try:
+                    if x_src == int(x_src) and y_src == int(y_src) and (x_src < W_src and x_src >= 0 and x_src < H_src and y_src >= 0):
                         warped_image[x, y, c] = source_image[x_src, y_src, c]
-                    except:
+                    else:
                         # if x_src, y_src are subpixel values or out of range will need to do something
                         # bilinear interpolation for inverse warp
-                        pass
-    
+                        x_ceil = int(np.ceil(x_src))
+                        x_floor = int(np.floor(x_src))
+                        y_ceil = int(np.ceil(y_src))
+                        y_floor = int(np.floor(y_src))
+                        ptA = (x_ceil, y_floor)
+                        ptB = (x_ceil, y_ceil)
+                        ptC = (x_floor, y_floor)
+                        ptD = (x_floor, y_ceil)
+                        pts = [ptA, ptB, ptC, ptD]
+                        for pt in pts:
+                            if pt[0] >= W_src or pt[0] < 0 or pt[1] >= H_src or pt[1] < 0:
+                                continue
+                            ratio = abs(x_src-pt[0]) * abs(y_src-pt[1])
+                            intensity = ratio * source_image[pt[0], pt[1], c]
+                            warped_image[x, y, c] += intensity
+
     return warped_image
-    
-                    
-
-
-    
